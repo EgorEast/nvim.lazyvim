721fee77fd927f905f89312a91652851ef01c1e7
egoreast <egoreast.true@gmail.com>
2025-02-20 12:48:36 +0300

    remove format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,11 +10,5 @@ return {
       typescriptreact = { "prettier" },
       javascriptreact = { "prettier" },
     },
-
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
   },
 }
