123cdcf21ffa3492cd54490ac7c20aa1f486f7c2
egoreast <egoreast.true@gmail.com>
2025-02-23 17:14:43 +0300

    added new recommended plugins, update live-share config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,20 +6,3 @@ local map = vim.keymap.set
 
 --map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save" })
-
--- Debug
-map("n", "<leader>du", function()
-  require("dapui").toggle()
-end, { desc = "Toggle Debug UI" })
-map("n", "<leader>db", function()
-  require("dap").toggle_breakpoint()
-end, { desc = "Toggle Breakpoint" })
-map("n", "<leader>ds", function()
-  require("dap").continue()
-end, { desc = "Start" })
-map("n", "<leader>dn", function()
-  require("dap").step_over()
-end, { desc = "Step Over" })
-
--- Eslint
-map("n", "<leader>lf", ":!eslint_d % --fix<CR>", { desc = "Eslint fix current buffer" })
--- lua/plugins/live-share.lua
+++ lua/plugins/live-share.lua
@@ -5,8 +5,17 @@ return {
       "jbyuki/instant.nvim",
     },
     keys = {
+      { "<leader>l", "", desc = "+Live Share" },
       { "<leader>ls", ":LiveShareServer<CR>", desc = "Start a Live Share server" },
       { "<leader>lj", ":LiveShareJoin", desc = "Join a Live Share session" },
     },
+    config = function()
+      vim.g.instant_username = "egoreast"
+      require("live-share").setup({
+        port_internal = 8765,
+        max_attempts = 40, -- 10 seconds
+        service = "serveo.net",
+      })
+    end,
   },
 }
