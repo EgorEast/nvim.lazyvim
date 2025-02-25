39302120bd7ea4e76166e897e4f445d6de7d17ae
egoreast <egoreast.true@gmail.com>
2025-02-25 09:37:57 +0300

    chore: comment jest neotest, enable wrap and add ru lang for spelling by default

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -6,14 +6,14 @@ return {
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-jest")({
-            jestCommand = "npm test --",
-            jestConfigFile = "jest.config.ts",
-            env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
-          }),
+          -- require("neotest-jest")({
+          -- jestCommand = "npm test --",
+          -- jestConfigFile = "jest.config.ts",
+          -- env = { CI = true },
+          -- cwd = function(path)
+          -- return vim.fn.getcwd()
+          -- end,
+          -- }),
           require("neotest-vitest")({
             -- Filter directories when searching for test files. Useful in large projects (see Filter directories notes).
             filter_dir = function(name, rel_path, root)
