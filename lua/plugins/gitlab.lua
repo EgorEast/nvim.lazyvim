db3f48d3683998c47cd1975219d7b91e48016e29
egoreast <egoreast.true@gmail.com>
2025-03-15 22:18:18 +0300

    complete gitlab config without credentials

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -11,6 +11,10 @@ return {
     require("gitlab.server").build(true)
   end, -- Builds the Go binary
   config = function()
-    require("gitlab").setup()
+    require("gitlab").setup({
+      auth_provider = function()
+        return "", "", nil
+      end,
+    })
   end,
 }
