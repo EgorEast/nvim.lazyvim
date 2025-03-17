6e01f6ae5611243c0ee36814f1eae1ffd738808c
egoreast <egoreast.true@gmail.com>
2025-03-17 13:16:49 +0300

    add gemini, enable sourcegraph, update plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -38,25 +38,24 @@ return {
       -- },
     },
   },
-  --
   -- https://github.com/sourcegraph/sg.nvim
-  -- {
-  --   "sourcegraph/sg.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-telescope/telescope.nvim",
-  --   },
-  --   config = function()
-  --     -- Sourcegraph configuration. All keys are optional
-  --     require("sg").setup({
-  --       -- Pass your own custom attach function
-  --       --    If you do not pass your own attach function, then the following maps are provide:
-  --       --        - gd -> goto definition
-  --       --        - gr -> goto references
-  --       -- on_attach = your_custom_lsp_attach_function
-  --     })
-  --   end,
-  -- },
+  {
+    "sourcegraph/sg.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      -- Sourcegraph configuration. All keys are optional
+      require("sg").setup({
+        -- Pass your own custom attach function
+        --    If you do not pass your own attach function, then the following maps are provide:
+        --        - gd -> goto definition
+        --        - gr -> goto references
+        -- on_attach = your_custom_lsp_attach_function
+      })
+    end,
+  },
   {
     "Exafunction/codeium.nvim",
     lazy = false,
@@ -72,4 +71,55 @@ return {
       })
     end,
   },
+  -- {
+  --   -- https://github.com/u3ih/gemini.nvim?ysclid=m8coek9gzf44878956
+  --   "gera2ld/ai.nvim",
+  --   dependencies = "nvim-lua/plenary.nvim",
+  --   opts = {
+  --     ---- AI's answer is displayed in a popup buffer
+  --     ---- Default behaviour is not to give it the focus because it is seen as a kind of tooltip
+  --     ---- But if you prefer it to get the focus, set to true.
+  --     result_popup_gets_focus = false,
+  --     ---- Override default prompts here, see below for more details
+  --     -- prompts = {},
+  --     ---- Default models for each prompt, can be overridden in the prompt definition
+  --     models = {
+  --       {
+  --         provider = "deepseek",
+  --         model = "deepseek-r1",
+  --         result_tpl = "## DeepSeek\n\n{{output}}",
+  --       },
+  --       -- {
+  --       --   provider = "gemini",
+  --       --   model = "gemini-pro",
+  --       --   result_tpl = "## Gemini\n\n{{output}}",
+  --       -- },
+  --       -- {
+  --       --   provider = "openai",
+  --       --   model = "gpt-3.5-turbo",
+  --       --   result_tpl = "## GPT-3.5\n\n{{output}}",
+  --       -- },
+  --     },
+  --
+  --     --- API keys and relavant config
+  --     -- gemini = {
+  --     --   api_key = "YOUR_GEMINI_API_KEY",
+  --     --   -- model = 'gemini-pro',
+  --     --   -- proxy = '',
+  --     -- },
+  --     -- openai = {
+  --     --   api_key = "YOUR_OPENAI_API_KEY",
+  --     --   -- base_url = 'https://api.openai.com/v1',
+  --     --   -- model = 'gpt-4',
+  --     --   -- proxy = '',
+  --     -- },
+  --   },
+  --   config = function()
+  --     local ai = require("ai")
+  --     local ok, opts = pcall(vim.fn.json_decode, os.getenv("AI_NVIM_PROVIDER_CONFIG"))
+  --     opts = ok and opts or {}
+  --     ai.setup(opts)
+  --   end,
+  --   event = "VeryLazy",
+  -- },
 }
