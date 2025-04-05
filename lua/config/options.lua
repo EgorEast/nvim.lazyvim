d0018eeccf1ccbc3015b64dee957ab0a91f894e1
egoreast <egoreast.true@gmail.com>
2025-04-05 18:07:42 +0300

    configure two languages to spelling
    update plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local config_path = vim.fn.stdpath("config")
 
 opt.spelllang = { "en", "ru" }
+opt.spellfile = config_path .. "/spell/en.utf-8.add" .. "," .. config_path .. "/spell/ru.utf-8.add"
 opt.spell = true
 opt.spellsuggest = "double"
 opt.wrap = true
