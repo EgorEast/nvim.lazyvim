203e1ca5738ebe4362981f1c4df90bd94a8190e5
egoreast <egoreast.true@gmail.com>
2025-05-29 12:02:50 +0300

    enable and configure render checkboxes in markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,27 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    checkbox = {
+      enabled = true,
+      right_pad = 6,
+      unchecked = {
+        -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+        -- icon = "󰄰",
+        -- Highlight for the unchecked icon.
+        -- highlight = "RenderMarkdownUnchecked",
+        -- Highlight for item associated with unchecked checkbox.
+        scope_highlight = "RenderMarkdownUnchecked",
+      },
+      checked = {
+        -- Replaces '[x]' of 'task_list_marker_checked'.
+        -- icon = "󰄳",
+        -- Highlight for the checked icon.
+        -- highlight = "RenderMarkdownChecked",
+        -- Highlight for item associated with checked checkbox.
+        scope_highlight = "RenderMarkdownChecked",
+      },
+    },
+  },
+}
