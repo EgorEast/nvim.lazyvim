d84afafeae77db77ad9826b5b01bcbd58bbe0c42
egoreast <egoreast.true@gmail.com>
2025-05-29 09:50:00 +0300

    add obsidian plugin

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -17,14 +17,14 @@ return {
     backend = "kitty",
     integrations = {
       markdown = {
-        enabled = true,
+        enabled = false,
         clear_in_insert_mode = false,
         download_remote_images = true,
         only_render_image_at_cursor = false,
         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
       },
       neorg = {
-        enabled = true,
+        enabled = false,
         clear_in_insert_mode = false,
         download_remote_images = true,
         only_render_image_at_cursor = false,
