0443afa09adf07042f0ba3e51eafa2e55a4c0be2
egoreast <egoreast.true@gmail.com>
2025-05-30 13:35:42 +0300

    configure creating note path func

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -175,5 +175,13 @@ return {
       return suffix
     end,
 
+    -- Optional, customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      -- This is equivalent to the default behavior.
+      local path = spec.dir / tostring(spec.id)
+      return path:with_suffix(".md")
+    end,
   },
 }
