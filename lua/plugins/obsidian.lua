da87614575d08fc67865359ac3043b310745c5da
egoreast <egoreast.true@gmail.com>
2025-05-30 13:45:09 +0300

    configure creating wikilinks func

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -183,5 +183,16 @@ return {
       local path = spec.dir / tostring(spec.id)
       return path:with_suffix(".md")
     end,
+
+    -- Optional, customize how wiki links are formatted. You can set this to one of:
+    --  * "use_alias_only", e.g. '[[Foo Bar]]'
+    --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+    --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+    --  * "use_path_only", e.g. '[[foo-bar.md]]'
+    -- Or you can set it to a function that takes a table of options and returns a string, like this:
+    wiki_link_func = function(opts)
+      -- return require("obsidian.util").wiki_link_id_prefix(opts)
+      return require("obsidian.util").wiki_link_alias_only(opts)
+    end,
   },
 }
